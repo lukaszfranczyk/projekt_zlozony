62c245e731c58c14a0bed27076ab163a66b23d3e
Wojtek <wtf.tux@gmail.com>
2016-09-14 22:57:07 +0200

    [US6] Profile editing

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -6,8 +6,29 @@ from .base_handler import BaseHandler
 
 class ProfileHandler(BaseHandler):
 
+    EDIT = '/profile/edit'
+
     @authenticated
     @coroutine
     def get(self, *args, **kwargs):
         user_data = yield self.db.get_user(user_name=self.current_user)
         self.render("profile.html", user_data=user_data)
+
+    @coroutine
+    def post(self, *args, **kwargs):
+        password = self.get_argument("password")
+        first_name = self.get_argument("first_name")
+        last_name = self.get_argument("last_name")
+        email = self.get_argument("email")
+        if self.request.uri.startswith(self.EDIT):
+            yield self.edit(password, first_name, last_name, email)
+        else:
+            self.redirect("/")
+
+    @coroutine
+    def edit(self, password, first_name, last_name, email):
+        user = self.get_current_user()
+        if user is None or password is None:
+            raise Exception("Error updating user: incomplete data")
+
+        yield self.db.update_user(user, password, first_name, last_name, email)
