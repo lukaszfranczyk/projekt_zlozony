22bfcb51279702aaf15978cf5c71384242da2896
Wojtek <wtf.tux@gmail.com>
2016-10-16 22:36:09 +0200

    Revert "[US6] Profile editing"
    This reverts commit b368b895ec4299f9587e07f0cd623ab00ab2cd51.

--- db/pgapi.py
+++ db/pgapi.py
@@ -52,15 +52,7 @@ class PgApi(metaclass=Collection):
         if cursor.rowcount == 0:
             return
 
-        return User(**cursor.fetchone())
+        temp = cursor.fetchone()
 
-    def update_user(self, user_name, password, first_name, last_name, email):
-        sql = 'UPDATE users SET password = %s, first_name = %s, last_name = %s, email = %s WHERE login = %s'
-        args = [password, first_name, last_name, email, user_name]
-        try:
-            yield self.db.execute(sql, args)
-            logging.info("User updated")
-        except Exception:
-            logging.exception("There was problem to update user %s", user_name)
-            return
+        return User(**temp)
 
